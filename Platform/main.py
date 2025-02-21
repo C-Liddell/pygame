1452665a752b3a73ac224a6aecd501046d2e25c0
C-Liddell <c.liddell@albynschool.org>
2025-02-21 09:52:39 +0000

    Plat Collisions

--- Platform/main.py
+++ Platform/main.py
@@ -8,34 +8,41 @@ screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 running = True
 
+maxY = height
+
 
 
 class character:
-    def __init__(self, x, y, width, height, colour, grounded):
+    def __init__(self, x, y, width, height, colour):
         self.pos = pygame.Vector2(x, y)
         self.vel = pygame.Vector2(0, 0)
         self.acc = pygame.Vector2(0, 0)
 
         self.width = width
         self.height = height
+        self.maxY = 720
         self.rect = pygame.Rect(self.pos.x, self.pos.y, self.width, self.height)
         
         self.colour = colour
 
         self.jump = 0
-        self.grounded = grounded
+        self.grounded = False
 
     def update(self):
         self.rect = pygame.Rect(self.pos.x, self.pos.y, self.width, self.height)
         pygame.draw.rect(screen, self.colour, self.rect)
 
 
-player = character(0, 0, 50, 80, "blue", False)
+player = character(0, 0, 50, 80, "blue")
+platform = [character(0, 700, width/2, 20, "black"), character(width/2 + 75, 650, width/2, 20, "black")]
+
+print(platform)
 
 
 
 def main():
     while running:
+        print(player.grounded)
 
         dt = clock.tick(30)
         screen.fill("purple")
@@ -47,8 +54,12 @@ def main():
 
 
         controller()
-
         player.update()
+
+        collision()
+
+        for p in platform:
+            p.update()
 
         pygame.display.flip()
 
@@ -56,8 +67,7 @@ def main():
 def controller():
     keys = pygame.key.get_pressed()
     speed = 10
-
-    gravity = 6
+    gravity = 10
 
     if keys[pygame.K_a]:
         player.pos.x -= speed
@@ -65,13 +75,10 @@ def controller():
         player.pos.x += speed
 
     if keys[pygame.K_SPACE] and player.grounded:
-        player.jump += 15
+        player.jump = 30
 
-    if player.pos.y == height - player.height:
-        player.grounded = True
-    else:
-        player.pos.y += gravity
-        player.grounded = False
+    
+    player.pos.y += gravity
 
 
     player.pos.y -= player.jump
@@ -81,7 +88,21 @@ def controller():
 
 
     player.pos.x = max(min(player.pos.x, width - player.width), 0)
-    player.pos.y = max(min(player.pos.y, height - player.height), 0)
+
+def collision():
+    plat_rect = []
+    for i in platform:
+        plat_rect.append(i.rect)
+
+    plat_index = player.rect.collidelist(plat_rect)
+
+    if plat_index != -1:
+        player.maxY = platform[plat_index].rect.top
+        player.grounded = True
+    else:
+        player.maxY = height
+        player.grounded = False
+    player.pos.y = max(min(player.pos.y, player.maxY - player.height), 0)
 
 
 main()
