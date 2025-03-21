143407fa9fb552e492fb7e07cdb5b45f8c780d08
C-Liddell <c.liddell@albynschool.org>
2025-03-21 09:59:12 +0000

    .

--- Platform/main.py
+++ Platform/main.py
@@ -12,8 +12,8 @@ running = True
 
 
 class Character:
-    def __init__(self, pos):
-        self.pos = pygame.Vector2(0, 0)
+    def __init__(self, x, y):
+        self.pos = pygame.Vector2(x, y)
         self.maxY = 720
         self.minY = 0
         self.grounded = False
@@ -33,11 +33,12 @@ class Character:
 
 
 class Platform:
-    colour = "black"
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, width, height, colour):
         self.pos = pygame.Vector2(x,y)
         self.width = width
         self.height = height
+
+        self.colour = colour
 
         self.rect = pygame.Rect(self.pos.x, self.pos.y, self.width, self.height)
 
@@ -47,17 +48,20 @@ class Platform:
 
 def loadLevel(lvlNo):
     platform = []
-    player = Character((0,0))
     with open(f"Platform/level{lvlNo}.txt", "r") as file:
         data = csv.reader(file)
+        player_pos = next(data)
+        player = Character(int(player_pos[0]), int(player_pos[1]))
+        finish_pos = next(data)
+        finish = Platform(int(finish_pos[0]), int(finish_pos[1]), int(finish_pos[2]), int(finish_pos[3]), finish_pos[4])
         for lines in data:
-            platform.append(Platform(int(lines[0]), int(lines[1]), int(lines[2]), int(lines[3])))
-    return platform, player
-
+            platform.append(Platform(int(lines[0]), int(lines[1]), int(lines[2]), int(lines[3]), lines[4]))
+    return platform, player, finish
         
 
 
-def main():
+def main(platform, player, finish):
+    levelCounter = 1
     while running:
         clock.tick(60)
 
@@ -65,19 +69,24 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
             
-        controller()
+        player = controller(player, platform)
 
         screen.fill("purple")
 
         player.update()
         for p in platform:
             p.update()
+        finish.update()
+
+        if player.rect.colliderect(finish.rect):
+            levelCounter += 1
+            platform, player, finish = loadLevel(levelCounter)
 
         pygame.display.flip()
 
 
 
-def controller():
+def controller(player, platform):
     keys = pygame.key.get_pressed()
 
     acceleration = 2
@@ -104,11 +113,12 @@ def controller():
     player.pos.y += player.vel.y
 
     player.pos.x = max(min(player.pos.x, width - player.width), 0)
-    collision()
+    player = collision(platform)
+    return player
 
 
 
-def collision():
+def collision(platform):
     plat_rect = [p.rect for p in platform]
     plat_index = player.rect.collidelist(plat_rect)
     if plat_index != -1:
@@ -123,7 +133,9 @@ def collision():
         player.minY = 0
         player.grounded = False
     player.pos.y = max(min(player.pos.y, player.maxY - player.rect.height), player.minY)
+    return player
 
 
-platform, player = loadLevel(1)
-main()
+
+platform, player, finish = loadLevel(1)
+main(platform, player, finish)
